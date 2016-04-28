693c7b850636845c962f2756a5a420c7ac7dab3a
Maximilian Witte <maximilian.t.witte@gmail.com>
2016-04-27 21:56:43 -0500

    Add /rsvp for testing DB connection

--- server.pl
+++ server.pl
@@ -1,14 +1,34 @@
 #!/usr/bin/env perl
 
+use lib "$FindBin::Bin/lib";
+
 use Mojo::UserAgent;
 use Mojolicious::Lite;
 use Mojo::Exception;
+use Mojo::JSON qw/ encode_json /;
 use DDP;
+
+use Wedding::Config;
+use Wedding;
 
 get '/' => sub {
 	my $c = shift;
 
 	$c->render( template => 'index' );
+};
+
+get '/rsvp' => sub {
+	my $c = shift;
+
+	my $rs = Wedding->_schema->resultset('Rsvp')->search();
+
+	my $data;
+	foreach my $row ( $rs->all() ) {
+		my %hash = $row->get_columns();
+		push @{$data}, \%hash;
+	}
+
+	$c->render( json => $data );
 };
 
 post '/rsvpsubmit' => sub {
