545529fc9de73e7c098b8c902cbcbcc6e06b980a
Maximilian Witte <maximilian.t.witte@gmail.com>
2016-05-01 20:42:31 -0500

    Add rsvp_attendee table

--- db/make_table.sql
+++ db/make_table.sql
@@ -2,7 +2,18 @@ DROP TABLE IF EXISTS rsvp;
 
 CREATE TABLE rsvp
 (
-	id int AUTO_INCREMENT PRIMARY KEY,
+	rsvp_id int AUTO_INCREMENT PRIMARY KEY,
 	rsvp_code INT NOT NULL,
-	has_responded BOOL NOT NULL DEFAULT 0
+	has_responded BOOL NOT NULL DEFAULT 0,
+	is_coming BOOL,
+	primary_contact varchar
+);
+
+CREATE TABLE rsvp_attendee
+(
+	rsvp_response_id int AUTO_INCREMENT PRIMARY KEY,
+	rsvp_id INT NOT NULL,
+	first_name varchar,
+	last_name varchar,
+	display_name varchar
 );
