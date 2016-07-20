b3bb9425d28305e65e48bc31430639f909cc6523
Maximilian Witte <maximilian.t.witte@gmail.com>
2016-07-20 09:39:52 -0500

    Add parking information for the structure.

--- public/js/LocationContent.jsx
+++ public/js/LocationContent.jsx
@@ -15,7 +15,18 @@ var LocationContent = React.createClass({
           </div>
           <div className="col-xs-6 vertical-split-left">
             <h3>Parking Info</h3>
-            Parking info goes here
+			<p>There is a major parking structure just around the corner from Cuvee.</p>
+
+			<p>
+			Information for this structure specifically is available <a href="http://www.parkmilwaukee.com/facilities/75-historic-third-ward-water-street-structure">here</a>.
+			There is also potentially street parking in the area however the 3rd Ward Art Festival is taking place at the same time as our wedding, so parking may be tight.  Because of this event, rates at the structure may also be higher than posted on the site...
+			</p>
+			<iframe
+			  width="450"
+			  height="250"
+			  frameBorder="0" style={{border:0}}
+              src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBiHjleP09CeFXW8mlt1XwY-1cHjZEZzJU&q=225 E Chicago St, Milwaukee, WI 53202">
+			</iframe>
           </div>
         </div>
       </div>
