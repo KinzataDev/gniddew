3ca1da3d2f2b209c743148497df69c81276a3b92
Maximilian Witte <maximilian.t.witte@gmail.com>
2016-08-02 09:22:19 -0500

    Change the Location page to be vertical

--- public/js/LocationContent.jsx
+++ public/js/LocationContent.jsx
@@ -4,7 +4,7 @@ var LocationContent = React.createClass({
       <div className="col-xs-12 buffer">
         <h2>Cuvee Champagne Lounge</h2>
         <div className="row">
-          <div className="col-xs-6 vertical-split-right">
+          <div className="col-xs-12">
             <p>Enter building, take elevator in the back to the third floor.</p>
             <iframe
               width="450"
@@ -13,7 +13,8 @@ var LocationContent = React.createClass({
               src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBiHjleP09CeFXW8mlt1XwY-1cHjZEZzJU&q=Cuvee,Milwaukee,WI">
             </iframe>
           </div>
-          <div className="col-xs-6 vertical-split-left">
+		  <hr/>
+          <div className="col-xs-12">
             <h3>Parking Info</h3>
 			<p>There is a major parking structure just around the corner from Cuvee.</p>
 
